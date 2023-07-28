29f74e361244d1a28658904316e6a8492aa61d45
hepo6479 <70782300+hepo6479@users.noreply.github.com>
2023-07-28 20:06:48 +0900

    Update music_transformer.py

--- model/music_transformer.py
+++ model/music_transformer.py
@@ -9,6 +9,7 @@ from utilities.device import get_device
 from .positional_encoding import PositionalEncoding
 from .rpr import TransformerEncoderRPR, TransformerEncoderLayerRPR
 
+import streamlit as st
 
 # MusicTransformer
 class MusicTransformer(nn.Module):
@@ -111,7 +112,7 @@ class MusicTransformer(nn.Module):
         # They are trained to predict the next note in sequence (we don't need the last one)
         return y
 
-    # generate
+  # generate
     def generate(self, primer=None, target_seq_length=1024, beam=0, beam_chance=1.0):
         """
         ----------
@@ -122,9 +123,10 @@ class MusicTransformer(nn.Module):
         ----------
         """
 
-        assert (not self.training), "Cannot generate while in training mode"
+        # assert (not self.training), "Cannot generate while in training mode"
 
         print("Generating sequence of max length:", target_seq_length)
+        st.write("Generating sequence of max length:", target_seq_length)
 
         gen_seq = torch.full((1,target_seq_length), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=get_device())
 
@@ -170,8 +172,71 @@ class MusicTransformer(nn.Module):
             cur_i += 1
             if(cur_i % 50 == 0):
                 print(cur_i, "/", target_seq_length)
+                st.write(cur_i, "/", target_seq_length)
 
         return gen_seq[:, :cur_i]
+
+    # # generate
+    # def generate(self, primer=None, target_seq_length=1024, beam=0, beam_chance=1.0):
+    #     """
+    #     ----------
+    #     Author: Damon Gwinn
+    #     ----------
+    #     Generates midi given a primer sample. Music can be generated using a probability distribution over
+    #     the softmax probabilities (recommended) or by using a beam search.
+    #     ----------
+    #     """
+
+    #     assert (not self.training), "Cannot generate while in training mode"
+
+    #     print("Generating sequence of max length:", target_seq_length)
+
+    #     gen_seq = torch.full((1,target_seq_length), TOKEN_PAD, dtype=TORCH_LABEL_TYPE, device=get_device())
+
+    #     num_primer = len(primer)
+    #     gen_seq[..., :num_primer] = primer.type(TORCH_LABEL_TYPE).to(get_device())
+
+
+    #     # print("primer:",primer)
+    #     # print(gen_seq)
+    #     cur_i = num_primer
+    #     while(cur_i < target_seq_length):
+    #         # gen_seq_batch     = gen_seq.clone()
+    #         y = self.softmax(self.forward(gen_seq[..., :cur_i]))[..., :TOKEN_END]
+    #         token_probs = y[:, cur_i-1, :]
+
+    #         if(beam == 0):
+    #             beam_ran = 2.0
+    #         else:
+    #             beam_ran = random.uniform(0,1)
+
+    #         if(beam_ran <= beam_chance):
+    #             token_probs = token_probs.flatten()
+    #             top_res, top_i = torch.topk(token_probs, beam)
+
+    #             beam_rows = top_i // VOCAB_SIZE
+    #             beam_cols = top_i % VOCAB_SIZE
+
+    #             gen_seq = gen_seq[beam_rows, :]
+    #             gen_seq[..., cur_i] = beam_cols
+
+    #         else:
+    #             distrib = torch.distributions.categorical.Categorical(probs=token_probs)
+    #             next_token = distrib.sample()
+    #             # print("next token:",next_token)
+    #             gen_seq[:, cur_i] = next_token
+
+
+    #             # Let the transformer decide to end if it wants to
+    #             if(next_token == TOKEN_END):
+    #                 print("Model called end of sequence at:", cur_i, "/", target_seq_length)
+    #                 break
+
+    #         cur_i += 1
+    #         if(cur_i % 50 == 0):
+    #             print(cur_i, "/", target_seq_length)
+
+    #     return gen_seq[:, :cur_i]
 
 # Used as a dummy to nn.Transformer
 # DummyDecoder
